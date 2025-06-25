fe086aca7602e50556c62d89e0770822eb74c0b2
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-06-25 14:22:53 +0530

    Create increasingPyramidPattern.java

---
+++ highend/increasingPyramidPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a num:");
+		int n = in.nextInt();
+		
+		for(int i=0;i<n;i++) {
+		    for(int j=1;j<=i+1;j++) {
+		        System.out.print(j + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
