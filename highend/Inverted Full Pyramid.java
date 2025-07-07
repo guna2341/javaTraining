2499e937e96186fd65f0f4eff3b7c83d16c42841
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-07-07 14:35:19 +0530

    Create Inverted Full Pyramid.java

---
+++ highend/Inverted Full Pyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+      int n = 5;
+        for(int i=n;i>=0;i--) {
+            for(int j=0;j<n-i;j++) {
+                System.out.print("  ");
+            }
+            for(int j=0;j<(i*2)-1;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
