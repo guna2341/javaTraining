f873a8ded726be51c16b6e45f19636d5ccb24c7a
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-07-07 15:21:19 +0530

    Create Triangular Pattern of Alphabets.java

---
+++ highend/Triangular Pattern of Alphabets.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter num:");
+        int n = in.nextInt();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n-i;j++) {
+                System.out.print((char)('A'+j+i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
