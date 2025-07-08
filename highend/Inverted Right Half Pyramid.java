a87412624d3ecefe88a5daa2bed7ab70036440a1
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-07-08 15:47:33 +0530

    Create Inverted Right Half Pyramid.java

---
+++ highend/Inverted Right Half Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter num:");
+        int n = in.nextInt();
+       for(int i=0;i<n;i++) {
+           for(int j=0;j<n-i;j++) {
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+    }
+}
+
+
+
