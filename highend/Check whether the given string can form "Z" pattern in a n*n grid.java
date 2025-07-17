d291e2ecbc08ac4b224ee59ea7190701608efe56
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-07-17 15:42:15 +0530

    Update Check whether the given string can form "Z" pattern in a n*n grid.java

--- highend/Check whether the given string can form "Z" pattern in a n*n grid.java
+++ highend/Check whether the given string can form "Z" pattern in a n*n grid.java
@@ -5,27 +5,32 @@ import java.util.Arrays;
 
 class test {
     public static void main(String[] args) {
-        String str = "zohocorporationteam";
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
         int l = str.length()+2;
         int n = 0;
-        for(int i=(l)/2;i>=0;i--) {
+        for(int i=(l)/2;i>=2;i--) {
             if (l%i==0) {
                 n = i;
                 break;
+            }
+            if (i == 2) {
+                System.out.println("Pattern cannot be formed");
+                return;
             }
         }
         int k = n;
         for(int i=0;i<str.length();i++) {
             if (i>n-1 && i < (n+n)-2) {
                 for(int j=0;j<(n+n)-i-2;j++) {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             if (i<n-1) {
-                System.out.print(str.charAt(i));
+                System.out.print(str.charAt(i) + " ");
             }
             else if (i>(n+n)-3) {
-                System.out.print(str.charAt(i));
+                System.out.print(str.charAt(i) + " ");
             }
             else {
                 System.out.println(str.charAt(i));
